0873165b55c200f0a0900fd17ad3d254a2b446d6
Kirby Esplana <kirby.esplana@dbtlabs.com>
2026-08-06 20:24:42 +0000

    specific columns

--- models/staging/stg_test_transform.sql
+++ models/staging/stg_test_transform.sql
@@ -1,4 +1,6 @@
 select
-    *,
-    first_name as first_name_raw --added comment
+    id,
+    first_name,
+    first_name as first_name_raw,
+    last_name
 from {{ source('jaffle_shop', 'customers') }}
